b8f3c674e34cb850a022d772dbec3b69271da1a6
Nisal-Sashmitha <IT20081034@my.sliit.lk>
2022-07-18 20:21:39 +0530

    done upto users connecting

--- index.js
+++ index.js
@@ -6,9 +6,30 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 const port = 3001
 
+const users = []
+
 app.get('/', (req, res) => {
   res.send('hello world');
 });
+ 
+const addUser = (userName, roomId)=>{
+  
+  users.push({
+    userName: userName,
+    roomId: roomId
+  })
+  console.log(getRoomUsers(roomId))
+
+}
+
+const getRoomUsers = (roomId) =>{
+  return users.filter(user=>(user.roomId == roomId))
+}
+
+
+const userLeave = (userName)=>{
+  users = users.filter(user=> user.userName != userName)
+}
 
 io.on('connection', (socket) => {
   console.log('a user connected');
@@ -16,6 +37,19 @@ io.on('connection', (socket) => {
     console.log('User joined room');
     console.log(roomId);
     console.log(userName);
+    socket.join(roomId);
+    addUser(userName,roomId)
+    socket.to(roomId).emit("user connected",userName);
+
+    io.to(roomId).emit("all-users", getRoomUsers(roomId));
+
+
+    socket.on("disconnect", ()=>{
+      console.log("disconnected");
+      socket.leave(roomId);
+      userLeave(userName)
+      io.to(roomId).emit("all-users", getRoomUsers(roomId))
+    })
   })
 });
 
